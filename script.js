894ff201062e3af8dec735465e818b9886387280
TOP <e227599@sbl.ch>
2022-12-11 16:47:30 +0100

    Side borderturn

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
 const circle = document.querySelector("#circ")
 let dy = 0
+let dx = 0
 
 function start() {
+    
+    dx = 1
+    
     window.requestAnimationFrame(update)
 }
 
@@ -17,8 +21,32 @@ function update() {
         dy = dy * (-1)
     }
     cy = cy + dy
+
     circle.setAttribute("cy", cy)
+
+    // Seitwärts Bewegung
+
+    let cx = parseFloat(circle.getAttribute("cx"))
+    cx = cx + dx
+
+    if (cx > 100){
+        dx = dx * (-1)
+    }
+
+    else if (cx < 0){
+        dx = dx * (-1)
+    }
+    cx = cx + dx
+
+    circle.setAttribute("cx", cx)
+
+
+
+
+
+
 
 
     window.requestAnimationFrame(update)
+
 }
